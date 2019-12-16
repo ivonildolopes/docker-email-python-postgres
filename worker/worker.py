3484ccd3ce3c67b1051aaff949eb2400fd2c6aba
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2019-12-15 23:26:02 -0300

    criando variavel de ambiente

--- worker/worker.py
+++ worker/worker.py
@@ -1,11 +1,15 @@
 import redis
 import json
+import os
 from time import sleep
 from random import randint
 
 if __name__ == '__main__':
-    r = redis.Redis(host='queue', port=6379, db=0)
+    redis_host = os.getenv('REDIS_HOST',"queue")
+    r = redis.Redis(host=redis_host, port=6379, db=0)
+
     print('Aguardando mensagem.........')
+    
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
         #similar o envio de email
